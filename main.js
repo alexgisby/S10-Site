70bd95eb4ff8a919660bf29f67408f0f87286cda
Alex Gisby <alex.gisby@gmail.com>
2011-12-10 16:03:51 +0000

    New S10 site started

---
+++ main.js
@@ -0,0 +1,56 @@
+/**
+ * JS file for S10
+ */
+
+$(function(){
+	
+	// Loop through the blades and assign some useful data:
+	var deg = 0, blades = $('section'), airbrush = $('#airbrush');
+	airbrush.data('currentRotation', 0);
+	blades.each(function(){
+		$(this).data('currentRotation', deg);
+		deg += 90;
+	});
+	
+	delete deg;
+	
+	$('.prev').click(function(e){
+		e.preventDefault();
+		spinBlades(90);
+	});
+	
+	$('.next').click(function(e){
+		e.preventDefault();
+		spinBlades(-90);
+	});
+});
+
+/**
+ * Spinning the blades
+ *
+ * @param 	int 	Amount to spin by
+ */
+function spinBlades(amount)
+{
+	var sections = $('section');
+	sections.each(function(){
+		var currentRot 	= $(this).data('currentRotation');
+		var newRotValue	= 'rotate(' + (currentRot + amount) +'deg)';
+		
+		$(this).css({
+			'-webkit-transform': newRotValue,
+			'-moz-transform': newRotValue
+		});
+		
+		$(this).data('currentRotation', (currentRot + amount));
+	});
+	
+	var airbrush 		= $('#airbrush');
+	var airCurrentRot 	= airbrush.data('currentRotation');
+	var newAirRot		= 'rotate(' + (airCurrentRot + amount) + 'deg)';
+	airbrush.css({
+		'-webkit-transform': newAirRot,
+		'-moz-transform': newAirRot
+	});
+	airbrush.data('currentRotation', (airCurrentRot + amount));
+}
